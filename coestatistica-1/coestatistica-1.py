c9226d972aadfd4768d32062ae25e005000feb42
Roberto <robertocsp95@gmail.com>
2020-06-24 10:54:18 -0300

    Desafio - Semana 3

---
+++ coestatistica-1/coestatistica-1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    df = pd.read_csv('desafio1.csv')
+    submit = df.groupby('estado_residencia')['pontuacao_credito'].agg([pd.Series.mode, 'median', 'mean', 'std']).T
+    submit.rename({'mode': 'moda', 'median': 'mediana', 'mean': 'media', 'std': 'desvio_padrao'})
+    submit.to_json('submission.json')
+    
+
+if __name__ == '__main__':
+    main()
+
